3122a070a023a33b3cb71dbbd1ea627337921255
Ariful Islam <iariful.info@gmail.com>
2022-02-09 00:22:42 +0600

    add some file

---
+++ src/Pages/Blog/blog.jsx
@@ -0,0 +1,11 @@
+
+
+const blog = () => {
+    return (
+        <div>
+            <h1>this is my first blog in ever in react js project that make my life too much fun</h1>
+        </div>
+    )
+}
+
+export default blog
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+import { AboutMe } from './AboutMe/about-me';
+import { Login } from './Login/login';
+import { Main } from './Main/main';
+import { Blog } from './Blog/blog';
+
+export { AboutMe, Login, Main, Blog };
